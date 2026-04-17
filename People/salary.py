3e71b41167fdca1704245afba0346805782f74fd
jeffkkiel <jeffezekielbelnas@gmail.com>
2026-04-17 16:07:03 +0800

    Create Salary class with initial methods

---
+++ People/salary.py
@@ -0,0 +1,19 @@
+class Salary:
+    def __init__(self):
+        self.salary_records = {}
+        self.hourly_rate = 67676767
+
+    def compute_daily_salary(self, employee_id, attendance_record):
+        pass
+
+    def apply_bonus(self, employee_id):
+        pass
+
+    def apply_deduction(self, employee_id):
+        pass
+
+    def update_salary(self, attendance):
+        pass
+
+    def show_salary_report(self):
+        pass
