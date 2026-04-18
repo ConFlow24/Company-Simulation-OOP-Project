69adb076187564efb712a7becc1fbce6bda1f618
ConFlow24 <jamatz.quintela@gmail.com>
2026-04-18 21:10:54 +0800

    Finished salary.py probably

--- Systems/salary.py
+++ Systems/salary.py
@@ -8,23 +8,33 @@ class Salary:
     def apply_bonus(self, employee, total_hours, original_salary):
         # If worked hours more than 12 hours add 15% for bonus.
         if total_hours > 12:
-            employee.pay += 10000
-            self.salary_record[employee]["Bonuses"] += 10000
+            employee.pay += 1000
+            self.salary_record[employee]["Bonuses"] += 1000
 
         return original_salary
 
     def apply_deduction(self, employee):
         if employee.late_count > 6 or employee.absent_count > 3:
-            employee.pay -= 10000
-            self.salary_record[employee]["Deductions"] -= 10000
+            employee.pay -= 1000
+            self.salary_record[employee]["Deductions"] -= 1000
         pass
 
-    def apply_monthly_bonus(self):
+    def apply_top5_bonus(self, top5_list, employees_list):
         # top 5 employees sa month may bonus
-        pass
+        for employee in top5_list:
+            if employee in employees_list:
+                employee.pay += 1000
+                self.salary_record[employee]["Bonuses"] += 1000
 
-    def show_salary_report(self, day, employees_list, ):
-        # Show salary, bonuss, deductions, and totals.
+    def apply_top5_deduct(self, bottom5_list, employees_list):
+        # top 5 employees sa month may bonus
+        for employee in bottom5_list:
+            if employee in employees_list:
+                employee.pay -= 1000
+                self.salary_record[employee]["Deductions"] -= 1000
+
+    def show_salary_report(self, employees_list):
         for employee in employees_list:
-            print 
-        pass
+            print(f"""Current salary: {employee.pay}
+Bonuses received over time: {self.salary_record[employee]["Bonuses"]}
+Deductions received over time: {self.salary_record[employee]["Bonuses"]}""")
