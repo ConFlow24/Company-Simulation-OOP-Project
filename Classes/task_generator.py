107b18e96df4b2f9750b560db14480a6d6a91e08
jeffkkiel <jeffezekielbelnas@gmail.com>
2026-04-29 11:25:11 +0800

    your message

--- Classes/task_generator.py
+++ Classes/task_generator.py
@@ -196,12 +196,14 @@ class TaskSystems:
                     self.completed_tasks.remove(task)
 
     def show_tasks(self):
-        print("\n--- Tasks ---")
+        print("\n" + "=" * 70)
+        print(f"{'TASKS FOR THE DAY':^70}")
+        print("=" * 70)
         print(
             f"{'Type':<10} {'Name':<15} {'Size':<10} {'Duration':>10} {'Assigned To':<20}")
-        print("-" * 70)
+        print("=" * 70)
         for task in self.task_list:
             assigned = task.assigned_to.name if task.assigned_to else "Unassigned"
             print(
                 f"{task.type:<10} {task.name.title():<15} {task.size:<10} {task.duration:>10} {assigned:<20}")
-        print("-" * 70 + "\n")
+        print("=" * 70 + "\n")
